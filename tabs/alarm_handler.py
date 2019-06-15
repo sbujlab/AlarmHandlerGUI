f516a2a57c7e6c9ae0b0120b0af82c43aef4160f
Cameron Clarke <cameronc137@gmail.com>
2019-06-15 10:07:19 -0400

    Updating actual alarm handler tab.... basically does everything.
    Now I just need to improve context menu on the Value = number bar
    and add a feature to display all contents of the clicked value

--- tabs/alarm_handler.py
+++ tabs/alarm_handler.py
@@ -27,13 +27,14 @@ class ALARM_HANDLER(tk.Frame):
     self.controlFrame = tk.LabelFrame(tab, text='Alarm Handler', background=u.grey_color)
     self.alarmFrame = tk.LabelFrame(tab, text='Alarm Handler Viewer', background=u.lightgrey_color)
     self.rowTitles = ["Alarms","ctd.","ctd.","ctd."]
-    self.NperCol = 8
+    self.NperRow = 4
+    self.rowsp = [1,1,1,1,1,1,1,1,1]
     self.colsp = [1,1,1,1]
     self.controlButtonsText = ["Alarm Status","Toggle Loop","Silence All","Refresh GUI"]
 
     self.alarmRows = []
     self.initialize_rows(OL)
-    self.buttons = []
+    self.displayFrames = []
     self.buttonMenus = []
     self.controlButtons = self.make_control_buttons(OL,fileArray, alarmLoop)
     self.make_screen(OL,fileArray)
@@ -95,87 +96,132 @@ class ALARM_HANDLER(tk.Frame):
       self.alarmRows[i].destroy()
     self.alarmRows = []
     self.initialize_rows(OL)
-    self.buttons = self.initialize_buttons(OL,fileArray)
+    self.displayFrames = self.initialize_displayFrames(OL,fileArray)
     self.buttonMenus = self.initialize_menus(OL,fileArray)
     self.alarmFrame.pack(padx=20,pady=10,anchor='nw')
+    self.erase_grid_all_row(OL,fileArray)
+    self.layout_grid_all_row(OL,fileArray)
 
   def initialize_rows(self,OL):
-    for i in range(0, len(self.rowTitles)):
+    for i in range(0, int(1.0*len(OL.objectList[2])/self.NperRow)+1):
       self.alarmRows.append(tk.LabelFrame(self.alarmFrame, text=self.rowTitles[i], background=u.lightgrey_color))
       self.alarmRows[i].grid(column=0,row=i,pady=10,padx=10,sticky='N')
 
-  def initialize_row(self,OL,rowI):
-    self.alarmRows[rowI] = tk.LabelFrame(self.alarmFrame, text=self.rowTitles[rowI], background=u.lightgrey_color)
-    self.alarmRows[rowI].grid(column=0,row=rowI,pady=10,padx=10,sticky='N')
-    self.alarmFrame.pack(padx=20,pady=10,anchor='nw')
-
-  def initialize_buttons(self,OL,fileArray): # Needs a short row to contain [name = value, alarm status = type, alarm stat !OK, user silence stat, alarm stat OK], context menu displays full parameter list
-    grid = []
+  def initialize_displayFrames(self,OL,fileArray): # Needs a short row to contain [name = value, alarm status = type, alarm stat !OK, user silence stat, alarm stat OK], context menu displays full parameter list
+    lgrid = []
     if len(OL.objectList)>(2) and len(OL.objectList[2])>0:
       for i in range(0,len(OL.objectList[2])):
-        # Loop over the list of objects, creating buttons
-        butt = tk.Button(self.alarmRows[int(1.0*i/self.NperCol)], text=OL.objectList[2][i].value, justify='center', background=OL.objectList[2][i].color) # loop over buttons
-        butt.indices = (int(1.0*i/self.NperCol),i)
+        # Loop over the list of objects, creating displayFrames
+        disp = tk.LabelFrame(self.alarmRows[int(1.0*i/self.NperRow)], text=OL.objectList[2][i].value, background=OL.objectList[2][i].color) # FIXME want red alarm full label frame?
+        disp.redStat = tk.IntVar()
+        disp.yellowStat = tk.IntVar()
+        disp.greenStat = tk.IntVar()
+        disp.alarmStatus = 1
+        disp.greenAlarmStatus = 0
+        disp.userSilenceStatus = 1
+        if OL.objectList[2][i].alarmStatus != "OK":
+          disp.alarmStatus = 0
+          disp.greenAlarmStatus = 1
+        if OL.objectList[2][i].userSilenceStatus != "Alert":
+          disp.userSilenceStatus = 0 
+        lgrid.append(disp)
+
+        butt = tk.Button(lgrid[i], text="Value = {}".format(OL.objectList[2][i].parameterList.get("Value",u.defaultKey)), justify='center', background=OL.objectList[2][i].color) # loop over displayFrames
+        butt.indices = (2,OL.objectList[2][i].columnIndex)
         butt.config(command = lambda but=butt: self.select_button(OL,fileArray,but))
-        grid.append(butt)
-      grid.append(grid)
-    return grid
+        butt.grid(columnspan=3, row=0,column=0)
+        radioButRed = tk.Radiobutton(lgrid[i], text=OL.objectList[2][i].alarmStatus, indicatoron=False, justify='left', value=lgrid[i].alarmStatus, variable=lgrid[i].redStat, fg=u.black_color, bg=u.lightgrey_color,
+            activebackground=u.grey_color, activeforeground=u.black_color, selectcolor = u.red_color, highlightbackground=u.red_color, highlightcolor=u.red_color, highlightthickness=2)
+        radioButRed.indices = (2,i)
+        radioButRed.config(command = lambda radRed=radioButRed: self.select_red_button(OL,fileArray,radRed))
+        radioButRed.grid(row=1,column=0)
+        radioButYellow = tk.Radiobutton(lgrid[i], text=OL.objectList[2][i].userSilenceStatus, indicatoron=False, justify='center', value=lgrid[i].userSilenceStatus, variable=lgrid[i].yellowStat, fg=u.black_color, bg=u.lightgrey_color,
+            activebackground=u.grey_color, activeforeground=u.black_color, selectcolor = u.yellow_color, highlightbackground=u.yellow_color, highlightcolor=u.yellow_color, highlightthickness=2)
+        radioButYellow.indices = (2,i)
+        radioButYellow.config(command = lambda radYellow=radioButYellow: self.select_yellow_button(OL,fileArray,radYellow))
+        radioButYellow.grid(row=1,column=1)
+        radioButGreen = tk.Radiobutton(lgrid[i], indicatoron=False, justify='right', value=lgrid[i].greenAlarmStatus, variable=lgrid[i].greenStat, fg=u.black_color, bg=u.lightgrey_color,
+            activebackground=u.grey_color, activeforeground=u.black_color, selectcolor = u.green_color, highlightbackground=u.green_color, highlightcolor=u.green_color, highlightthickness=2)
+        radioButGreen.indices = (2,i)
+        print("OL 2,{} alarm status = {}".format(i,OL.objectList[2][i].alarmStatus))
+        if OL.objectList[2][i].alarmStatus == "OK":
+          radioButGreen.config(text="OK")
+        else:
+          radioButGreen.config(text="Alarm")
+        radioButGreen.config(command = lambda radGreen=radioButGreen: self.select_green_button(OL,fileArray,radGreen))
+        radioButGreen.grid(row=1,column=2)
+    return lgrid
 
   def initialize_menus(self,OL,fileArray):
     grid = []
     #print("Adding menus, len(self.alarmRows) = {} times".format(len(self.alarmRows)))
-    for i in range(0, len(self.buttons)):
+    for i in range(0, len(self.displayFrames)):
       if len(OL.objectList[2])>=i:
-        buttMenu = tk.Menu(self.buttons[i], tearoff=0) # Is having the owner be button correct?
-        buttMenu.indices = (i,OL.objectList[2][i].columnIndex)
+        buttMenu = tk.Menu(self.displayFrames[i], tearoff=0) # Is having the owner be button correct?
+        buttMenu.indices = (2,OL.objectList[2][i].columnIndex)
         buttMenu.moveN = 0
         buttMenu.editValue = None
         buttMenu.add_command(label = 'Information', command = lambda butMenu = buttMenu: self.button_information_menu(OL,fileArray,butMenu))
         buttMenu.add_command(label = 'Silence', command = lambda butMenu = buttMenu: self.button_silence_menu(OL,fileArray,butMenu))
-        self.buttons[i].bind("<Button-3>",lambda event, butMenu = buttMenu: self.do_popup(event,butMenu))
+        self.displayFrames[i].bind("<Button-3>",lambda event, butMenu = buttMenu: self.do_popup(event,butMenu))
       grid.append(buttMenu)
     return grid
 
   def do_popup(self,event,butMenu):
     butMenu.tk_popup(event.x_root,event.y_root,0)
 
-  def layout_grid_all_row(self,rowID,OL,fileArray,newButt):
-    newButt.grid(row=0,column=rowID,columnspan=self.rowsp[rowID],padx=10,pady=10,sticky='N')
-    for i in range(0,len(self.buttons)):
-      # FIXME Put the int() mod math to place buttons in a given row here
-      self.buttons[i].grid(row=i,column=rowID,columnspan=self.rowsp[rowID],padx=10,pady=10,sticky='N')
+  def layout_grid_all_row(self,OL,fileArray):
+    for i in range(0,len(self.displayFrames)):
+      self.displayFrames[i].grid(row=int(1.0*i/self.NperRow),column=i%self.NperRow,rowspan=self.rowsp[int(1.0*i/self.NperRow)],padx=10,pady=10,sticky='N')
     #self.buttonMenus = self.initialize_menus(OL,fileArray)
 
-  def erase_grid_row(self,rowID,OL,fileArray,newButt):
+  def erase_grid_all_row(self,OL,fileArray):
     # FIXME rowID is not being parsed correctly here !!
-    for i in range(rowID,len(self.buttons)):
-      self.buttons[i].grid_forget()
-
-  def layout_grid_row(self,rowID,OL,fileArray):
-    # FIXME Put the int() mod math to place buttons in a given row here
-    for i in range(rowID,len(self.buttons)): 
-      self.buttons[i].grid_forget()
-    for j in range(rowID,len(self.buttons)):#NEW
-      self.alarmRows[j].grid_forget() #NEW
-    # Want to ask OL which object is clicked
-    # Then get the index range that its children live in
-    # Then grid those children (and erase prior grid, preserving creatorButton[rowdID])
-    #print("Adding row {}".format(rowID))
-    self.alarmRows[rowID].grid(column=0,row=rowID,pady=10,padx=10,sticky='N')#NEW
-    if rowID == 3 and len(self.alarmRows)>rowID+1: #NEW
-      #print("Adding row {}".format(rowID+1))
-      self.alarmRows[rowID+1].grid(column=0,row=rowID+1,pady=10,padx=10,sticky='N')#NEW
-    self.alarmFrame.pack(padx=20,pady=10,anchor='nw') #NEW
+    for i in range(0,len(self.displayFrames)):
+      self.displayFrames[i].grid_forget()
 
   def refresh_button(self,OL,fileArray,but):
     i,j = but.indices
     OL.set_clicked(i,j) # Update that object's color to dark grey
-    self.buttons[i].config(background=OL.objectList[i][j].color) # Update that button to be the newly update object's new color (could just use but.config)
+    self.displayFrames[i].config(background=OL.objectList[i][j].color) # Update that button to be the newly update object's new color (could just use but.config)
 
   def select_button(self,OL,fileArray,but):
+    # FIXME Put simple alarm handler behavior in here
     i,j = but.indices
     OL.set_clicked(i,j) # Update that object's color to dark grey
-    self.buttons[i].config(background=OL.objectList[i][j].color) # Update that button to be the newly update object's new color (could just use but.config)
+    self.displayFrames[i].config(background=OL.objectList[i][j].color) # Update that button to be the newly update object's new color (could just use but.config)
+
+  def select_red_button(self,OL,fileArray,but):
+    i,j = but.indices
+    alStat = 1
+    if OL.objectList[i][j].alarmStatus != "OK":
+      alStat = 0
+    but.config(value=alStat)
+    self.update_GUI(OL,fileArray)
+
+  def select_yellow_button(self,OL,fileArray,but):
+    i,j = but.indices
+    if OL.objectList[i][j].userSilenceStatus == "Silenced":
+      OL.objectList[i][j].userSilenceStatus = "Alert" 
+    elif OL.objectList[i][j].userSilenceStatus == "Alert":
+      OL.objectList[i][j].userSilenceStatus = "Silenced" 
+      OL.objectList[i][j].color = u.darkgrey_color
+    for q in range(OL.objectList[i][j].indexStart,OL.objectList[i][j].indexEnd+1):
+      if fileArray.filearray[q][3] == "User Silence Status": # Update the filearray too
+        fileArray.filearray[q][4] = OL.objectList[i][j].userSilenceStatus
+    silStat = 1
+    if OL.objectList[i][j].userSilenceStatus != "Alert":
+      silStat = 0
+    but.config(value=silStat)
+    self.update_GUI(OL,fileArray)
+
+  def select_green_button(self,OL,fileArray,but):
+    i,j = but.indices
+    alStat = 1
+    if OL.objectList[i][j].alarmStatus == "OK":
+      alStat = 0
+    but.config(value=alStat)
+    self.update_GUI(OL,fileArray)
 
   def update_GUI(self,OL,fileArray):
     fileArray.filearray = u.write_filearray(fileArray)
@@ -190,4 +236,3 @@ class ALARM_HANDLER(tk.Frame):
   def refresh_screen(self,OL,fileArray,alarmLoop):
     self.controlButtons = self.make_control_buttons(OL,fileArray, alarmLoop)
     self.update_GUI(OL,fileArray)
-
