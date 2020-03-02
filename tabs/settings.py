05e7dac774e3c4249800d8d5f7fce342525b5baf
Cameron Clarke <cameronc137@gmail.com>
2020-03-02 06:07:24 -0500

    Fixed settings tab -> it won't blink any more

--- tabs/settings.py
+++ tabs/settings.py
@@ -26,6 +26,10 @@ class SETTINGS(tk.Frame):
       self.buttons[k].grid(row=k,column=0,padx=5,pady=2,sticky='W')
       self.buttons[k].button.grid(row=0,column=0,padx=5,pady=2,sticky='W')
 
+  def update_settings(self,alarmHandlerWindow):
+    for k in range(0,len(self.buttons)):
+      self.buttons[k].button.config(text=alarmHandlerWindow.conf.conf[self.buttons[k].button.key])
+
   def erase_settingsFrame(self):
     for k in range(0,len(self.buttons)):
       self.buttons[k].grid_forget()
@@ -47,13 +51,13 @@ class SETTINGS(tk.Frame):
       lgrid[k].button = tk.Button(lgrid[k], text=localSlist[key], background=u.lightgrey_color)
       lgrid[k].button.index = k
       lgrid[k].button.key = key
-      lgrid[k].button.config(command = lambda but=lgrid[k].button: self.edit_setting(but,alarmHandlerWindow.conf.conf))
+      lgrid[k].button.config(command = lambda but=lgrid[k].button: self.edit_setting(but,alarmHandlerWindow.conf.conf,alarmHandlerWindow))
       lgrid[k].grid(row=k,column=0,padx=5,pady=2,sticky='W')
       lgrid[k].button.grid(row=0,column=0,padx=5,pady=2,sticky='W')
       k+=1
     return lgrid
 
-  def edit_setting(self,but,conf):
+  def edit_setting(self,but,conf,alarmHandlerWindow):
     lk = but.index
     key = but.key
     localStr = simpledialog.askstring("Input", "New setting value: ",parent = self.buttons[lk]) 
@@ -61,9 +65,11 @@ class SETTINGS(tk.Frame):
     u.write_conf(conf)
     self.buttons[lk].button.config(text = localStr)
     print(conf)
-    self.make_screen()
+    self.update_settings(alarmHandlerWindow)
+    #self.make_screen()
     
   def refresh_screen(self,alarmHandlerWindow):
     u.update_config(alarmHandlerWindow)
-    self.make_screen()
+    self.update_settings(alarmHandlerWindow)
+    #self.make_screen()
 
